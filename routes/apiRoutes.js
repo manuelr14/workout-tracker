f451a113e6567c28807c87009b0aae435e0e2f3e
Manuel Ramirez <manuelramirez1411@gmail.com>
2020-04-12 21:33:13 -0400

    routes modified

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,21 +1,31 @@
-var db = require("../models");
+
+
+  
+const db = require('../models/workout');
 
 module.exports = function(app) {
-  app.get("/api/workouts", function(req, res) {
-    db.Workout.find({}).then(function(dbPosts) {
-      res.json(dbPosts);
-    });
-  });
+	app.get('/api/workouts', function(req, res) {
+		db.find({}).then(function(dbWorkouts) {
+			res.json(dbWorkouts);
+		});
+	});
+	app.get('/api/workouts/range', function(req, res) {
+		db.find({}).then(function(dbWorkouts) {
+			res.json(dbWorkouts);
+		});
+	});
 
-  app.put("/api/workouts/:id", function(req, res) {
-    db.Workout.updateOne({ _id: req.params.id }).then(function(dbPost) {
-      res.json(dbPost);
-    });
-  });
-  app.get("/api/workouts/range", function(req, res) {
-    db.Workout.find({}).then(function(dbPosts) {
-      res.json(dbPosts);
-    });
-  });
- 
+	app.post('/api/workouts', (req, res) => {
+		db.create({}).then((dbWorkouts) => {
+			res.json(dbWorkouts);
+		});
+	});
+	app.put('/api/workouts/:id', (req, res) => {
+		db
+			.update({ _id: req.params.id }, { $push: { exercises: req.body } })
+			.then((data) => {
+				res.json(data);
+			});
+	});
 };
+
